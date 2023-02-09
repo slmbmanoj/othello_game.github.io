09493585762d8aea83b3fb40cc8fdd11a2392c77
slmbmanoj <124852763+slmbmanoj@users.noreply.github.com>
2023-02-09 09:18:44 +0530

    block.js was created and edited

---
+++ block.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+class Cell {
+
+    constructor(x, y, owner = null) {
+        this.x = x;
+        this.y = y;
+        this._owner = owner;
+    }
+
+    pos() {
+        return [this.x, this.y]
+    }
+
+    get owner() {
+        return this._owner;
+    }
+
+    set owner(owner) {
+        this._owner = owner;
+    }
+
+    neighbors() {
+        return [
+            [this.x - 1, this.y - 1], [this.x - 1, this.y], [this.x - 1, this.y + 1],
+            [this.x, this.y - 1], [this.x, this.y + 1],
+            [this.x + 1, this.y - 1], [this.x + 1, this.y], [this.x + 1, this.y + 1],
+        ];
+    }
+
+}
